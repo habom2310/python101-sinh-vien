ee3555710d61be34e7c81f06ce0f77af078548cb
Ha Nguyen <habom2310@gmail.com>
2021-11-27 12:41:07 +1100

    upload bai 13

---
+++ Code buoi hoc/Buoi 13/buoi_13.py
@@ -0,0 +1,23 @@
+# Buổi 13 - Environment - Package management
+
+## Cách tạo environment trong anaconda
+'''
+- Tạo environment: conda create -n <tên environment> python=<python version>
+VD: conda create -n py37 python=3.7
+- khởi động environment: conda activate <tên environment>
+
+'''
+
+## Cách download package trong anaconda
+'''
+- sử dụng pip install: pip install <package name>
+VD: pip install numpy
+'''
+
+## Cách sử dụng jupyter notebook trong anaconda
+'''
+- Install thư viện: pip install jupyter
+- khởi động notebook: jupyter notebook
+- sau khi browser mở jupyter thì tạo mới file .ipynb
+
+'''
